7d8ced02b25337b946d74af97c8ef7ad8c45bf7c
amapic <amaury.pichat@gmail.com>
2014-11-19 21:42:50 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,41 @@
 ## functions do
 
 ## Write a short comment describing this function
+## this function just check if the matrix is square, the rest is the same as the example function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if (nrow(x)!=ncol(x)) {
+    print("! the matrix is not square")
+    return()
+  }
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
+## the only difference with cacheMean is that m solve data
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
